b126722936737345b255c908c2c3009c1b9b4804
Merlin Mason <info@merlinmason.co.uk>
2014-06-04 18:37:37 +0100

    add favicons path

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,7 +119,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: "img/",
-                    src: ["*.{png,jpg,gif}"],
+                    src: ["*.{png,jpg,gif}", "favicons/*.{png,jpg,gif}"],
                     dest: "img/"
                 }]
             }
